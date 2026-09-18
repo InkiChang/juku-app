f32664d6791ff9c578c5d931422c3ce5ca2afaeb
InkiChang <inkichang@InkiChangdeMacBook-Pro.local>
2026-09-18 23:09:50 +0800

    fix: render icon assets as inline SVG

--- src/components/AppIcon.vue
+++ src/components/AppIcon.vue
@@ -4,8 +4,9 @@ import { icons, type IconName } from '../config/icons';
 
 const props = withDefaults(defineProps<{ name: IconName; label?: string; size?: number }>(), { size: 24 });
 const source = computed(() => icons[props.name]);
+const markup = computed(() => source.value.replace(/#333/g, 'currentColor'));
 </script>
 
 <template>
-  <span class="asset-icon" :style="{ '--icon-url': `url(${source})`, width: `${size}px`, height: `${size}px` }" :role="label ? 'img' : undefined" :aria-label="label"></span>
+  <span class="asset-icon" :style="{ width: `${size}px`, height: `${size}px` }" :role="label ? 'img' : undefined" :aria-label="label" v-html="markup"></span>
 </template>
--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,17 +1,17 @@
-import download from '../../icon/下载_download.svg?url';
-import lightTheme from '../../icon/亮色模式.svg?url';
-import about from '../../icon/关于.svg?url';
-import refresh from '../../icon/刷新_refresh.svg?url';
-import library from '../../icon/剧库.svg?url';
-import mine from '../../icon/我的_people.svg?url';
-import darkTheme from '../../icon/暗色模式.svg?url';
-import server from '../../icon/服务器_server.svg?url';
-import userManagement from '../../icon/用户管理.svg?url';
-import history from '../../icon/观看记录.svg?url';
-import settings from '../../icon/设置_setting-two.svg?url';
-import accountSecurity from '../../icon/账号安全.svg?url';
-import following from '../../icon/追剧.svg?url';
-import home from '../../icon/首页_home-two.svg?url';
+import download from '../../icon/下载_download.svg?raw';
+import lightTheme from '../../icon/亮色模式.svg?raw';
+import about from '../../icon/关于.svg?raw';
+import refresh from '../../icon/刷新_refresh.svg?raw';
+import library from '../../icon/剧库.svg?raw';
+import mine from '../../icon/我的_people.svg?raw';
+import darkTheme from '../../icon/暗色模式.svg?raw';
+import server from '../../icon/服务器_server.svg?raw';
+import userManagement from '../../icon/用户管理.svg?raw';
+import history from '../../icon/观看记录.svg?raw';
+import settings from '../../icon/设置_setting-two.svg?raw';
+import accountSecurity from '../../icon/账号安全.svg?raw';
+import following from '../../icon/追剧.svg?raw';
+import home from '../../icon/首页_home-two.svg?raw';
 
 export const icons = {
   home,
